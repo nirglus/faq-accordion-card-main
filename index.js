6adfeefeecc931c0c5ddc0c745e6465aaef6dacc
nirglus <nirgluskin@gmail.com>
2023-09-11 00:38:37 +0300

    add questPara with JS instead of HTML

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-
 function openScroll(){
+    var paraArr = [
+        `You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.`,
+        `No more than 2GB. All files in your account must fit your allotted storage space.`,
+        `Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.`,
+        `Yes! Send us a message and we’ll process your request no questions asked.`,
+        `Chat and email support is available 24/7. Phone lines are open during normal business hours`
+    ]
     var qHeadings = document.getElementsByClassName("questHeading");
     var isScrollActive = false;
     for(let i = 0; i < qHeadings.length; i++){
@@ -12,6 +18,7 @@ function openScroll(){
                 qHeadings[i].style.fontWeight = "700";
                 document.getElementsByClassName("arrow")[i].style = "transform: rotate(180deg)";
                 document.getElementsByClassName("questPara")[i].style.display = "block";
+                document.getElementsByClassName("questPara")[i].innerHTML= paraArr[i];
             }
             isScrollActive = !isScrollActive;
         })
