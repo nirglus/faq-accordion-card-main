9d86010ca219f7937450795cb1a5cd89f12bbf05
nirglus <nirgluskin@gmail.com>
2023-09-11 00:30:04 +0300

    change qHeadings var

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 
 function openScroll(){
-    var qHeadings = document.getElementsByClassName("arrow");
+    var qHeadings = document.getElementsByClassName("questHeading");
     var isScrollActive = false;
     for(let i = 0; i < qHeadings.length; i++){
         qHeadings[i].addEventListener("click", function(){
             if(isScrollActive){
-                document.getElementsByClassName("questHeading")[i].style.fontWeight = "400";
-                qHeadings[i].style = "transform: rotate(0deg)";
+                qHeadings[i].style.fontWeight = "400";
+                document.getElementsByClassName("arrow")[i].style = "transform: rotate(0deg)";
                 document.getElementsByClassName("questPara")[i].style.display = "none";
             }else{
-                document.getElementsByClassName("questHeading")[i].style.fontWeight = "700";
-                qHeadings[i].style = "transform: rotate(180deg)";
+                qHeadings[i].style.fontWeight = "700";
+                document.getElementsByClassName("arrow")[i].style = "transform: rotate(180deg)";
                 document.getElementsByClassName("questPara")[i].style.display = "block";
             }
             isScrollActive = !isScrollActive;
