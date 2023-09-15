15eaba687d1ae82c6146199f95f6a8081c9e5703
nirglus <nirgluskin@gmail.com>
2023-09-15 19:56:31 +0300

    change vars to let/const

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function openScroll(){
-    var paraArr = [
+    const PARA_ARR = [
         `You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.`,
         `No more than 2GB. All files in your account must fit your allotted storage space.`,
         `Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.`,
         `Yes! Send us a message and we’ll process your request no questions asked.`,
         `Chat and email support is available 24/7. Phone lines are open during normal business hours`
     ]
-    var qHeadings = document.getElementsByClassName("questHeading");
-    var isScrollActive = false;
+    let qHeadings = document.getElementsByClassName("questHeading");
+    let isScrollActive = false;
     for(let i = 0; i < qHeadings.length; i++){
         qHeadings[i].addEventListener("click", function(){
             if(isScrollActive){
@@ -23,7 +23,7 @@ function openScroll(){
                 qHeadings[i].style.fontWeight = "700";
                 document.getElementsByClassName("arrow")[i].style = "transform: rotate(180deg)";
                 document.getElementsByClassName("questPara")[i].style.display = "block";
-                document.getElementsByClassName("questPara")[i].innerHTML= paraArr[i];
+                document.getElementsByClassName("questPara")[i].innerHTML = PARA_ARR[i];
             }
             isScrollActive = !isScrollActive;
         })
